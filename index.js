bb05c9be151b798779aa7c2435b1c813d4ea0c60
efatha <163284022+efatha@users.noreply.github.com>
2024-09-25 14:37:25 +0200

    Made the close button being functional and

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ menu.addEventListener(click, efa)
 
 function efa(){
     document.getElementById("menuDisplay").innerHTML=`
-    <ul class="ul">
-      <button class="xbtn">x</button>
+    <ul class="ul" id="sidebar" style="transform: translateX(20%)">
+      <button class="xbtn" onclick="closeMenu()">x</button>
       <div class="menuPadding">
         <li class="li0"><a href="#">Home</a></li>
         <li class="li0"><a href="#">New</a></li>
@@ -14,4 +14,7 @@ function efa(){
       </div>
     </ul>`;
     document.getElementById("mySidebar").style.visibility="hidden";
+}
+function closeMenu() {
+  document.getElementById("sidebar").style.visibility="hidden";
 }
